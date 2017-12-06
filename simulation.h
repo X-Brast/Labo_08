c62dbe881cabe39bac20d7ecf60f8a9c956e7fba
mauricelehmann <m.lehmann94@gmail.com>
2017-12-06 09:30:25 +0100

    ajoute les enum de status

--- simulation.h
+++ simulation.h
@@ -16,11 +16,11 @@ const int axeX = 5 ;
 const int axeY = 5 ;
 
 //Enum des status
-   //Status de l'explorateur ( 1: OK , 2: Perdu , 3: riche , 4: Epuisé , 5:noyé  )
+//Status de l'explorateur ( 1: OK , 2: Perdu , 3: riche , 4: Epuisé , 5:noyé  )
    
-enum class Status { OK , PERDU , RICHE , EPUISE , NOYE } ;
+enum Status { OK , PERDU , RICHE , EPUISE , NOYE } ;
 
-   
+
 
 void initialisationPosition( int& positionX , int& positionY  , int& nbMouvement , int& status );
 void avancerCase( int& positionX , int& positionY ) ;
