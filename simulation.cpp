66831baba2da5c1cb77790952bc4f25218e3e705
mauricelehmann <m.lehmann94@gmail.com>
2017-12-10 17:42:33 +0100

    changé un petit truc dans controlCase

--- simulation.cpp
+++ simulation.cpp
@@ -12,14 +12,6 @@
  Compilateur : MinGW-g++ 6.3.0
  -----------------------------------------------------------------------------------
 */
-
-
-/*
- * Status de l'explorateur ( 1: OK , 2: Perdu , 3: riche , 4: Epuisé , 5:noyé  )
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 #include "simulation.h"
 #include "map.h"
 #include "aleatoire.h"
@@ -59,79 +51,43 @@ void avancerCase( int& positionX , int& positionY ){
    }
 }
 
-void controleCase( int positionX , int positionY , int& status , int map[][LARGEUR_MAP] ){
+void controleCase( int explorateur[][ATTRIBUTS], int map[][LARGEUR_MAP] ){
+   
+   int posX = explorateur[0][Attributs::positionX] ;
+   int posY = explorateur[0][Attributs::positionY] ;
    
    //Si la position est en dehors de la map
-   if( positionX < 0 or positionY < 0 or positionX > LONGUEUR_MAP or positionY > LARGEUR_MAP ){
-      status = Status::PERDU ;
+   if( posX < 0 
+      or posY < 0 
+      or posY > LONGUEUR_MAP 
+      or posX > LARGEUR_MAP ){
+      
+      //L'explorateur est perdu
+      explorateur[0][Attributs::status] = Status::PERDU ;
    }
   
-   switch( map[positionX][positionY]){
+   //Si l'explorateur est épuisé
+   if( explorateur[0][Attributs::nbMouvement] >=  LARGEUR_MAP * LONGUEUR_MAP ){
+         //L'explorateur est épuisé
+         explorateur[0][Attributs::status]  = Status::EPUISE ;
+      }
+   
+   switch( map[posX][posY]){
       //Si l'explorateur est tombé dans un lac
       case int(ListeObjet::LAC) :
-         status = Status::NOYE ;
+         explorateur[0][Attributs::status] = Status::NOYE ;
          break ;
       
       //Si l'explorateur est tombé sur le trésor
       case int(ListeObjet::TRESOR) :
-         status = Status::RICHE ;
+         explorateur[0][Attributs::status] = Status::RICHE ;
          break ;
    } 
 }
-/*
-void lancerSimulation( int& positionX , int& positionY , int& status , int& nbMouvement , int map[][LARGEUR_MAP] ){
-   
-   //Initialisation de la position
-   initialisationPosition( positionX , positionY , nbMouvement , status ) ;
-   
-   //Init du random
-   initialiserAleatoire() ;
-   
-   //L'explorateur marche jusqu'a changer de status ( epuisé , noyé , perdu , riche )
-   do{
-      //Controle si l'explorateur est epuisé
-      if( nbMouvement >=  LARGEUR_MAP * LONGUEUR_MAP ){
-         status = Status::EPUISE ;
-         break ;
-      }
-
-      //Avancer d'une case
-      avancerCase(positionX , positionY ) ;
-      nbMouvement ++ ;
-      cout << endl << "position x: " << positionX << endl << "position y : " << positionY << endl ;
-      
-      //On controle si l'explorateur n'est pas mort , noyé , riche ou épuisé
-      controleCase( positionX , positionY , status, map ) ;
-      //Si l'explorateur n'est pas OK , on arrête la simulation
-      if( status != Status::OK ){
-         break ;
-      }
-   }while( true ) ;
-}
-*/
 
 void lancerSimulation( int explorateur[][ATTRIBUTS] , int map[][LARGEUR_MAP] ){
    
-   //Init de l'explorateur : 
-   // [x][0] : position X
-   // [x][1] : position Y
-   // [x][2] : status
-   // [x][3] : nombre de mouvement
-   
-   
-//   int positionX = explorateur[0][0] ;
-//   int positionY = explorateur[0][1] ;
-//   int status = explorateur[0][2] ;
-//   int nbMouvement = explorateur[0][3];
-//   
-   /*
-   explorateur[0][Attributs::positionX]
-   explorateur[0][Attributs::positionY]
-   explorateur[0][Attributs::status] 
-   explorateur[0][Attributs::nbMouvement]
-   */
    //Initialisation de la position
-   //initialisationPosition( positionX , positionY , nbMouvement , status ) ;
    initialisationExplorateur( explorateur ) ;
    
    //Init du random
@@ -139,28 +95,23 @@ void lancerSimulation( int explorateur[][ATTRIBUTS] , int map[][LARGEUR_MAP] ){
    
    //L'explorateur marche jusqu'a changer de status ( epuisé , noyé , perdu , riche )
    do{
-      //Controle si l'explorateur est epuisé
-      if( explorateur[0][Attributs::nbMouvement] >=  LARGEUR_MAP * LONGUEUR_MAP ){
-         explorateur[0][Attributs::status]  = Status::EPUISE ;
+      
+      //Controle sur quelle case l'explorateur est
+      controleCase( explorateur , map ) ;
+      
+      //Si l'explorateur n'est pas OK , on arrête la simulation
+      if( explorateur[0][Attributs::status] != Status::OK ){
          break ;
       }
 
       //Avancer d'une case
       avancerCase(explorateur[0][Attributs::positionX] , explorateur[0][Attributs::positionY] ) ;
       
+      //Incrémentation du nombre de pas effectués
       explorateur[0][Attributs::nbMouvement] ++ ; 
       
       cout << endl << "position x: " << explorateur[0][Attributs::positionX] << endl 
               << "position y : " << explorateur[0][Attributs::positionY] << endl ;
       
-      //On controle si l'explorateur n'est pas mort , noyé , riche ou épuisé
-      controleCase( explorateur[0][Attributs::positionX] , 
-                    explorateur[0][Attributs::positionY] , 
-                    explorateur[0][Attributs::status], map ) ;
-      
-      //Si l'explorateur n'est pas OK , on arrête la simulation
-      if( explorateur[0][Attributs::status] != Status::OK ){
-         break ;
-      }
    }while( true ) ;
 }
--- simulation.h
+++ simulation.h
@@ -23,18 +23,10 @@
 
 using namespace std ; 
 
-
-//Enum des status
-//Status de l'explorateur ( 0: OK , 1: Perdu , 2: riche , 3: Epuisé , 4:noyé  )
-/*enum Status { OK , PERDU , RICHE , EPUISE , NOYE } ;
-enum Attributs { positionX , positionY , status , nbMouvement } ;*/
-
-
 void initialisationPosition( int& positionX , int& positionY  , int& nbMouvement , int& status );
 void initialisationExplorateur( int explorateur[][ATTRIBUTS] ) ;
 void avancerCase( int& positionX , int& positionY ) ;
-void controleCase( int positionX , int positionY , int& status );
-//void lancerSimulation( int& positionX , int& positionY , int& status , int& nbMouvement , int map[LONGUEUR_MAP][LARGEUR_MAP] );
+void controleCase( int explorateur[][ATTRIBUTS], int map[][LARGEUR_MAP] );
 void lancerSimulation( int explorateur[][ATTRIBUTS] , int map[][LARGEUR_MAP] );
    
 #endif /* SIMULATION_H */
