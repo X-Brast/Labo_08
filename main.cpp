abe94bb120a4ab7f8cee8dbe22b0e2e7a077222a
mauricelehmann <m.lehmann94@gmail.com>
2017-12-11 11:47:13 +0100

    main et stat

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include "variable.h"
 #include "simulation.h"
+#include "statistique.h"
 #include "saisie.h"
 #include "map.h"
 
---
+++ statistique.cpp
@@ -0,0 +1,15 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : Labo_08
+ Fichier     : statistique.cpp
+ Auteur(s)   : Alexandre Marques & Maurice Lehmann
+ Date        : 05.12.2017
+
+ But         : 
+
+ Remarque(s) : 
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+*/
+#include "statistique.h"
