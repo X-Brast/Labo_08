bf95d3e9bb98dc84a39787408a78b9c768c95009
mauricelehmann <m.lehmann94@gmail.com>
2017-12-12 10:03:00 +0100

    Rajoute commentaires

--- main.cpp
+++ main.cpp
@@ -5,9 +5,17 @@
  Auteur(s)   : Alexandre Marques & Maurice Lehmann
  Date        : 05.12.2017
 
- But         : 
+ But         : Programme permettant de simuler un explorateur en quete de trésors.
+ *             Le programme génère une carte 2 dimensions ( un tableau ), contenant différentes
+ *             valeurs : la terre ferme ( par défaut ) , des lacs , un trésor.
+ *             L'utilisateur est invité à entrer un nombre n de simulations, correspondant
+ *             au nombre d' "essai" d'un unique explorateur.
+ *             
+ *             Les données relatives au parcours de l'explorateur sont finalement anaylisées
+ *             et affichées. Cela comprend :
+ *             - La probabilité pour un explorateur de trouver le trésor.
+ *             - Le nombre moyen de pas nécessaires de ceux qui l’ont trouvé
 
- Remarque(s) : 
 
  Compilateur : MinGW-g++ 6.3.0
  -----------------------------------------------------------------------------------
@@ -21,17 +29,16 @@
 #include "saisie.h"
 #include "map.h"
 
-//using namespace std;
-
 int main() 
 {
 
    //Tableau d'information de l'explorateur :
    int explorateur[NB_UTILISATEUR][Attributs::NB_ATTRIBUTS] ;
      
+   //Initialisation de la carte
    int map[LONGUEUR_MAP][LARGEUR_MAP] = {};
    
-   // la terre n'étant pas un objet nous deveons la retirer
+   // La terre n'étant pas un objet, on la retire
    int nbObjet = ListeObjet::NB_OBJET - 1;
    
    cout << "=======================================================================" << endl
@@ -42,21 +49,29 @@ int main()
    
    do
    {
+      //Saisie utilisateur
       int nbSimulation = saisir(1,100000, "Veuillez inserer le nombre de simluation voulu");
       int historiqueEvenement[nbSimulation][StatEvent::NB_STAT];
       
+      //On réinitialise la carte
       ReinitialiserCarte(map);
       
+      //On crée la carte, avec les positions des objets 
       creationCarte(map, nbObjet, explorateur);
       
+      //Affichage de la carte
       afficherMap(map);
       
+      //Affichage des coordonees initiales de l'explorateur
       cout << "Coordonees initiales (x ; y) de l'explorateur : ( " << explorateur[0][0] << " ; " << explorateur[0][1] << " )" << endl;
       
+      //On lance la simulation, un nombre n fois ( nbSimulation )
       lancerSimulation( explorateur , map , historiqueEvenement , nbSimulation );
-         
+      
+      //On affiche finalement les statistiques
       afficherStatistique( historiqueEvenement , nbSimulation ) ;
       
+   //Si l'utilisateur veut recommencer le programme
    }while(recommencerProgramme());
   
    return EXIT_SUCCESS;
