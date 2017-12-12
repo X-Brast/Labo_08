1dcf5d87a5738ec22af2ddacea7128ea8c98c5ba
X-Brast <alx-brast@outlook.com>
2017-12-12 10:15:40 +0100

    commentaire pour les fichier map

--- map.cpp
+++ map.cpp
@@ -5,7 +5,7 @@
  Auteur(s)   : Alexandre Marques & Maurice Lehmann
  Date        : 05.12.2017
 
- But         : 
+ But         : permet de créer un map avec different objet 
 
  Remarque(s) : 
 
@@ -16,38 +16,44 @@
 #include "map.h"
 #include "simulation.h"
 
-void ReinitialiserCarte(int tab[][LARGEUR_MAP])
+
+void ReinitialiserCarte(int map[][LARGEUR_MAP])
 {
-   for(int x = 0; x < LONGUEUR_MAP; ++x)
+   for(int posX = 0; posX < LONGUEUR_MAP; ++posX)
    {
-      for(int y = 0; y < LARGEUR_MAP; ++y)
+      for(int posY = 0; posY < LARGEUR_MAP; ++posY)
       {
-         tab[x][y] = ListeObjet::TERRE;
+         // on tamisse la map avec de la terre ainsi il n'y aura plus d'autre objet
+         map[posX][posY] = ListeObjet::TERRE;
       }
    }
 }
 
-void afficherMap(int tab[][LARGEUR_MAP])
+void afficherMap(int map[][LARGEUR_MAP])
 {
-   for(int x = 0; x < LONGUEUR_MAP; ++x)
+   for(int posX = 0; posX < LONGUEUR_MAP; ++posX)
    {
       cout << endl << " | ";
       
-      for(int y = 0; y < LARGEUR_MAP; ++y)
+      for(int posY = 0; posY < LARGEUR_MAP; ++posY)
       {
-         cout << tab[x][y] << " | ";
+         cout << map[posX][posY] << " | ";
       }
    }
-   
    cout << endl;
 }
 
 void creationCarte(int map[][LARGEUR_MAP], int nbObjet, int tabUtilisateur[][Attributs::NB_ATTRIBUTS])
 {
+   // on initilise l'aleatoire car nous allons l'utiliser plusieur fois dans cette fonction   
    initialiserAleatoire();
    
+   // on définit le nombre de lacs voulu
    int nbLacs = 3;
    
+   // on a utilisé un switch afin de permettre dans le futur de choisir combien d'objet on voudrait.
+   // on devra commencé par creer les objets terrain (ex: lac) avant de creer les objets matériels.
+   // ATTENTION !!! seul contraint est que l'utilisateur doit être creer en dernier car il ne sera pas representer dans la map
    switch(ListeObjet(nbObjet))
    {
       case ListeObjet::LAC:
@@ -59,46 +65,50 @@ void creationCarte(int map[][LARGEUR_MAP], int nbObjet, int tabUtilisateur[][Att
       // toujours faire en dernier
       case ListeObjet::UTILISATEUR:
          creationUtilisateur(map ,tabUtilisateur);
-         
-      default:
-         break;
    }
 }
 
 void creationLac(int tab[][LARGEUR_MAP], int rayon, int nbLac)
 {
+   // nous vérifions que le rayon n'est pas nulle avant créer des lacs
    if(rayon > 0)
    {
-      // nous avons définit que le rayon 0 vaut 0 case dans le tableau
+      // nous desincrementons le rayon car nous allons définir un centre plus bas et qu'ildeviendra la distance avec ce centre
       rayon--;
       
+      // création de plusieur lacs
       for(int nbIteration = 0; nbIteration < nbLac; ++nbIteration)
       {
+         // nous générons les positions des lacs en X et Y
          int posX_Lac = nombreAleatoire(LONGUEUR_MAP - 1);
          int posY_Lac = nombreAleatoire(LARGEUR_MAP - 1);
          
-         // verification du centre avant de lancer toutes les vérifications du lac
-         if(tab[posX_Lac][posY_Lac] != 0)
+         // verifie si la position du centre du lacs n'a pas déjà un objet
+         if(tab[posX_Lac][posY_Lac])
          {
+            // la création de ce lac a été un echec alors on recommence
             nbIteration--;
             continue;
          }
 
+         // verifie si la futur position du lac n'a pas déjà des objets
          if(!verificationObjetLargeMap(tab, posX_Lac, posY_Lac, rayon))
          {
+            // la création de ce lac a été un echec alors on recommence
             nbIteration--;
             continue;
          }
          
-         //cout << posX_Lac << " - " << posY_Lac << endl;
-         
-         for(int x = 0-rayon; x <= rayon; ++x)
+         // nous parcourons toutes les cellules de la map par raport au rayon du lac
+         for(int posX = 0-rayon; posX <= rayon; ++posX)
          {
-            for(int y = 0-rayon; y <= rayon; ++y)
+            for(int posY = 0-rayon; posY <= rayon; ++posY)
             {
-               if(abs(x) + abs(y) <= rayon && (posX_Lac + x) >= 0 && (posX_Lac + x) < LONGUEUR_MAP && (posY_Lac + y) >= 0 && (posY_Lac + y) < LARGEUR_MAP)
+               // nous vérifions si l'insertion n'est pas à l'extérieur de la map 
+               // et que la somme des positions en valeur absolu est inférieur ou égal au rayon car nous voulons representer un cercle pixeliser
+               if(abs(posX) + abs(posY) <= rayon && (posX_Lac + posX) >= 0 && (posX_Lac + posX) < LONGUEUR_MAP && (posY_Lac + posY) >= 0 && (posY_Lac + posY) < LARGEUR_MAP)
                {
-                  tab[posX_Lac + x][posY_Lac + y] = (int)ListeObjet::LAC;
+                  tab[posX_Lac + posX][posY_Lac + posY] = (int)ListeObjet::LAC;
                }
             }
          }
@@ -110,16 +120,18 @@ void creationLac(int tab[][LARGEUR_MAP], int nbLac)
 {
    int rayon = 0;
    
+   // créations de plusieurs lacs
    for(int numLac = 1; numLac <= nbLac; ++numLac)
    {
+      // nous allons générer le rayon par rapport au plus petit des cotés de la map
       if(LONGUEUR_MAP < LARGEUR_MAP)
       {
-         rayon = nombreAleatoire(((int)LARGEUR_MAP / 3) + 1, 1);
+         rayon = nombreAleatoire(((int)LONGUEUR_MAP / 3) + 1, 1);
          creationLac(tab, rayon, 1);
       }
       else
       {
-         rayon = nombreAleatoire(((int)LONGUEUR_MAP / 3) + 1, 1);
+         rayon = nombreAleatoire(((int)LARGEUR_MAP / 3) + 1, 1);
          creationLac(tab, rayon, 1);
       }
    }
@@ -127,34 +139,44 @@ void creationLac(int tab[][LARGEUR_MAP], int nbLac)
 
 void creationTresor(int tab[][LARGEUR_MAP], int nbTresor)
 {
+   // création de plusieur trésor
    for(int nbIteration = 0; nbIteration < nbTresor; ++nbIteration)
    {
+      // on génére la position futur du tresor en X et Y
       int posX_Tresor = nombreAleatoire(LONGUEUR_MAP - 1);
       int posY_Tresor = nombreAleatoire(LARGEUR_MAP - 1);
 
-      if(tab[posX_Tresor][posY_Tresor] != 0)
+      // on vérifie si un objet existe à cette position
+      if(tab[posX_Tresor][posY_Tresor])
       {
+         // la création de ce tresor a été un echec alors on recommence
          nbIteration--;
          continue;
       }
 
+      // on insert le tresor sur la map
       tab[posX_Tresor][posY_Tresor] = (int)ListeObjet::TRESOR;
    }
 }
 
-void creationUtilisateur(int tab[][LARGEUR_MAP], int tabUtilisateur[][Attributs::NB_ATTRIBUTS], int nbUtilisateur)
+void creationUtilisateur(int map[][LARGEUR_MAP], int tabUtilisateur[][Attributs::NB_ATTRIBUTS], int nbUtilisateur)
 {
+   // création de plusieur utilisateur
    for(int nbIteration = 0; nbIteration < nbUtilisateur; ++nbIteration)
    {
+      // nous générons la position de l'utilisateur en X et Y
       int posX_Utilisateur = nombreAleatoire(LONGUEUR_MAP -1);
       int posY_Utilisateur = nombreAleatoire(LONGUEUR_MAP -1);
 
-      if(tab[posX_Utilisateur][posY_Utilisateur] != 0 && caseUtiliserParUtilisateur(tabUtilisateur, posX_Utilisateur, posY_Utilisateur))
+      // on vérifie si un objet existe à cette position
+      if(map[posX_Utilisateur][posY_Utilisateur]/* || caseUtiliserParUtilisateur(tabUtilisateur, posX_Utilisateur, posY_Utilisateur)*/)
       {
+         // la création de cet utilisateur a été un echec alors on recommence
          nbIteration--;
          continue;
       }
       
+      // on insert la position initial de l'utilisateur dans un tableau et on initialise son status et nombre de pas
       tabUtilisateur[nbIteration][Attributs::positionX]     = posX_Utilisateur;
       tabUtilisateur[nbIteration][Attributs::positionY]     = posY_Utilisateur;
       tabUtilisateur[nbIteration][Attributs::status]        = Status::OK;
@@ -162,10 +184,13 @@ void creationUtilisateur(int tab[][LARGEUR_MAP], int tabUtilisateur[][Attributs:
    }
 }
 
+// non utiliser dans ce projet
 bool caseUtiliserParUtilisateur(const int tabUtilisateur[][Attributs::NB_ATTRIBUTS], const int posX, const int posY, const int nbUtilisateur)
 {
+   // on parcourt le tableau utilisateur 
    for(int nbIteration = 0; nbIteration < nbUtilisateur; ++nbIteration)
    {
+      // on vérifie si un utilisateur n'existe pas déjà à cette emplacement
       if(tabUtilisateur[nbIteration][Attributs::positionX] == posX && tabUtilisateur[nbIteration][Attributs::positionY] == posY)
       {
          return true;
@@ -177,12 +202,15 @@ bool caseUtiliserParUtilisateur(const int tabUtilisateur[][Attributs::NB_ATTRIBU
 
 bool verificationObjetLargeMap(const int tab[][LARGEUR_MAP], const int& posX, const int& posY, const int& rayon)
 {
+   // nous parcourons toutes les cellules de la map par raport au rayon du lac
    for(int x = 0-rayon; x <= rayon; ++x)
    {
       for(int y = 0-rayon; y <= rayon; ++y)
       {
+         // on verifie que la somme des positions en valeur absolu est inférieur ou égal au rayon car nous voulons representer un cercle pixeliser
          if(abs(x) + abs(y) <= rayon)
          {
+            // nous vérifions qu'il n'existe pas des objets sur la map
             if((posX + x) >= 0 && (posX + x) < LONGUEUR_MAP && (posY + y) >= 0 && (posY + y) < LARGEUR_MAP && tab[posX + x][posY + y])
             {
                return false;
--- map.h
+++ map.h
@@ -5,9 +5,10 @@
  Auteur(s)   : Alexandre Marques & Maurice Lehmann
  Date        : 05.12.2017
 
- But         : 
+ But         : Déclaration des fonctions et constantes de map.cpp 
 
- Remarque(s) : 
+ Remarque(s) : le fichier variable.h doit toujours être presentes car il y a des enums et variable important pour la map
+ *             La fonction caseUtiliserParUtilisateur n'est pas utilisé dans ce projet mais il est utile si on voudrait plusieurs utilisateurs dans la map
 
  Compilateur : MinGW-g++ 6.3.0
  -----------------------------------------------------------------------------------
@@ -23,22 +24,75 @@
 
 using namespace std;
 
-//enum class ListeObjet {TERRE, UTILISATEUR, TRESOR, LAC};
-
-/*const int LONGUEUR_MAP = 10;
-const int LARGEUR_MAP = 10;
-const int ATTRIBUTS = 4 ;
-const int NB_UTILISATEUR = 0 ; */
-
+/**
+ * permet de reinitialiser le tableau avec des valeurs à zéro
+ * @param tab : tableau contenant toutes les informations de la map
+ */
 void ReinitialiserCarte(int tab[][LARGEUR_MAP]);
+
+/**
+ * permet d'afficher la map
+ * @param tab : tableau contenant toutes les informations de le map
+ */
 void afficherMap(int tab[][LARGEUR_MAP]);
 
+/**
+ * permet de creer un carte dans un tableau donnée
+ * @param tab           : tableau qui contiendra toutes les informations. Le tableau doit être reinitialiser avant de lancer cette fonction
+ * @param nbObjet       : permet d'afficher un nombre d'objet different voulu en se basant du enum ListeObjet
+ * @param tabUtilisateur: tableau qui contiendra tous les utilisateurs de la map
+ */
 void creationCarte(int tab[][LARGEUR_MAP], int nbObjet, int tabUtilisateur[][Attributs::NB_ATTRIBUTS]);
+
+/**
+ * permet de creer des lacs (surcharge)
+ * @param tab  : tableau contenant les informations de la map
+ * @param nbLac: nombre de lacs qu'on voudrait afficher dans la map
+ */
 void creationLac(int tab[][LARGEUR_MAP], int nbLac);
+
+/**
+ * ppermet de creer des lacs avec un rayon défini (surcharge)
+ * @param tab  : tableau contenant les informations de la map
+ * @param rayon: le rayon des lacs voulus
+ * @param nbLac: nombre de lacs qu'on voudrait afficher dans la map
+ */
 void creationLac(int tab[][LARGEUR_MAP], int rayon, int nbLac);
+
+/**
+ * premet de créer un nombre de tresor sur la map
+ * @param tab      : tableau contenant les informations de la map
+ * @param nbTresor : le nombre de tresor voulu dans la map
+ */
 void creationTresor(int tab[][LARGEUR_MAP], int nbTresor = 1);
+
+/**
+ * permet de creer un nombre d'utilisateur sur la map
+ * @param tab            : tableau contenant les informations de la map
+ * @param tabUtilisateur : tableau qui contiendra les utilisateur crees sur la map
+ * @param nbUtilisateur  : le nombre utilisateur voulu dans la map ( on définit une valeur par defaut a NB_UTILISATEUR+1, car
+ *                                                                   un tableau commence à 0 alors qu'on minimum 1 )
+ */
 void creationUtilisateur(int tab[][LARGEUR_MAP], int tabUtilisateur[][Attributs::NB_ATTRIBUTS], int nbUtilisateur = NB_UTILISATEUR+1);
+
+/**
+ * permet de vérifier si la position de départ de l'utilisateur est déjà utilié par un autre (Non utilisé dans ce projet)
+ * @param tabUtilisateur : tableau contenant les informations des differents utilisateurs
+ * @param posX           : position en x où l'utilisateur devrait commencer
+ * @param posY           : position en y où l'utilisateur devrait commencer
+ * @param nbUtilisateur  : nombre d'utilisateur dans le tableau
+ * @return               : return true si un utilisateur existe déjà dans à cette position
+ */
 bool caseUtiliserParUtilisateur(const int tabUtilisateur[][Attributs::NB_ATTRIBUTS], const int posX, const int posY, const int nbUtilisateur = NB_UTILISATEUR+1);
+
+/**
+ * permet de verifier si la création du lacs ne chevauchent pas un autre objet de la map
+ * @param tab   : tableau contenant les informations de la map
+ * @param posX  : position en X du centre du lacs
+ * @param posY  : position en Y du centre du lacs
+ * @param rayon : le rayon du lac
+ * @return      : return true si il n'y a aucun objet sur la map où le lac va être creer
+ */
 bool verificationObjetLargeMap(const int tab[][LARGEUR_MAP], const int& posX, const int& posY, const int& rayon = 1);
 
 #endif /* MAP_H */
